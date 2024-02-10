f5af9ca459e22833312c4e983d06201dca36bf1c
sahil <sahilmulani501@gmail.com>
2024-02-10 12:43:43 +0530

    rollback : back to previous cookie method

---
+++ .changeset/mean-students-kneel.md
@@ -0,0 +1,5 @@
+---
+"redshield": patch
+---
+
+back to previous cookie method
--- actions/login.ts
+++ actions/login.ts
@@ -1,10 +1,11 @@
 "use server";
+import { cookies } from "next/headers";
 import { getEnv } from "./auth";
 
 export const LoginUser = async ({
   email,
   password,
-  project_id
+  project_id,
 }: {
   email: string;
   password: string;
@@ -23,7 +24,7 @@ export const LoginUser = async ({
       body: JSON.stringify({
         email: email.trim(),
         password: password.trim(),
-        project_id: project_id
+        project_id: project_id,
       }),
     });
     const response = (await res.json()) as {
@@ -31,6 +32,9 @@ export const LoginUser = async ({
       message: string;
       token: string;
     };
+    if(response.status){
+      setJWTcookie({token: response.token});
+    }
     return {
       status: response.status || false,
       message: response.message || "something went wrong",
@@ -44,3 +48,14 @@ export const LoginUser = async ({
   }
 };
 
+const setJWTcookie = ({ token }: { token: string }) => {
+  const cookieStore = cookies();
+  const date = new Date();
+  date.setTime(date.getTime() + 7 * 24 * 60 * 60 * 1000);
+
+  cookieStore.set("_auth_token", token, {
+    expires: date,
+    httpOnly: true,
+    sameSite: true,
+  });
+};
